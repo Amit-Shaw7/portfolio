f65098e95b987e47be81e227b464380a1c3a7630
Amit-Shaw7 <maahiamit777@gmail.com>
2022-12-01 02:02:09 +0530

    fixed favicon

--- src/pages/home/Hero.js
+++ src/pages/home/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
                 <p className='text-center text-4xl'>Hello<span className='text-red-500'>...🙋‍♂️</span></p>
                 <p className='text-center mt-4 sm:text-6xl text-4xl'>I am &nbsp;&nbsp;<span className='text-red-500 font-extrabold font-mono tracking-wider'>AMIT KUMAR SHAW</span>, </p>
                 <p className='text-center mt-4 sm:text-4xl text-2xl text-slate-600 font-semibold'>A FULL STACK WEB DEVELOPER</p>
-                <a href='#hire' className='rounded-md mt-8 px-3 py-2 bg-red-500 text-black font-bold text-xl custom-transition hover-property font-mono'>Get in touch</a>
+                <a href='#contact' className='rounded-md mt-8 px-3 py-2 bg-red-500 text-black font-bold text-xl custom-transition hover-property font-mono'>Get in touch</a>
                 {/* <button className='rounded-md mt-8 text-white bg-slate-700 font-bold text-2xl custom-transition  font-mono flex items-center'><FaLinkedin/></button>
                 <button className='rounded-md mt-8 text-white bg-slate-700 font-bold text-2xl custom-transition  font-mono flex items-center'><FaLinkedin/></button> */}
             </div>
