f0976a8cc2b7c7d422ddb555ef606c6685a3b829
Amit-Shaw7 <maahiamit777@gmail.com>
2023-05-28 18:01:26 +0530

    increased padding

--- src/pages/home/Skills.js
+++ src/pages/home/Skills.js
@@ -9,7 +9,7 @@ const Skills = () => {
       <div className='h-[100%] w-[95%] flex items-center justify-center flex-wrap mt-8'>
         {
           skillArr.map((skill) => (
-            <div key={skill.id} className='tooltip relative flex items-center justify-center mx-10 my-10 md:mx-16 h-[90px] w-[90px] md:h-[120px] md:w-[120px] border-[1px] border-slate-700 p-2 rounded-full'>
+            <div key={skill.id} className='tooltip relative flex items-center justify-center mx-10 my-10 md:mx-16 h-[90px] w-[90px] md:h-[120px] md:w-[120px] border-[1px] border-slate-700 p-3 rounded-full'>
               <img src={require("../../assets/skills/" + skill.img)} className={`${skill?.name === "STYLED COMPONENTS" && "bg-black"} h-[80%] rounded-xl object-contain" alt="Tailwind`} alt={skill?.name}/>
               <span className='custom-transition uppercase bg-slate-500 p-1 rounded-md hidden absolute top-[-40px] text-white tooltip-text'>{skill?.name}</span>
             </div>
